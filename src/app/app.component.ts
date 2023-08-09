795c3d27e31cfdc0e917a3ccdd47d51bf217bffb
Ethan McDowell <ethan.r.mcdowell@gmail.com>
2023-08-08 20:58:57 -0400

    notes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'map-project';
+
+  // OK so ideas for this project:
+  // https://medium.com/geekculture/firebase-and-angular-connect-your-frontend-app-to-a-nosql-database-quickies-1-974c298316a9
+  // try using the restcountries.com api endpoint
+  // and load the info up that I will want to display
+  // into a database - when you click on a country display
+  // a pop-up showing the country-specific data
+  // use firebase to host app
 }
